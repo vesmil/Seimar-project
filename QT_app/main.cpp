be9ef3ddeab43659ce7a586c63e732defe142c98
Milan Veselý <vesely202@gmail.com>
2022-07-13 15:08:25 +0000

    Removed preparations

--- QT_app/main.cpp
+++ QT_app/main.cpp
@@ -4,13 +4,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
 	const string videoSrcPath {"/dev/video0"};
-
 	QGuiApplication app(argc, argv);
-	/* The pipeline must be initilalized after, as the qt controls the event loops.
-	 * Also the pipeline must before qml is loaded, because gstreamer loads qmlglsink plugin. */
 }
