d2642ffd55434dc7568d861368eff791b0e93f3d
Milan Veselý <vesely202@gmail.com>
2022-08-16 12:36:29 +0200

    README and code style details

--- zynq_app/library/visca/visca.cpp
+++ zynq_app/library/visca/visca.cpp
@@ -4,15 +4,20 @@
 #include "global/logCategories.h"
 #include "viscaCommands.h"
 
-Visca::Visca(const char *device_path) : m_uart(device_path) {
-    for (int i = 0; i < INIT_TRIES_COUNT; i++) {
-        if (!setAddress()) {
+Visca::Visca(const char *device_path) : m_uart(device_path)
+{
+    for (int i = 0; i < INIT_TRIES_COUNT; i++)
+    {
+        if (!setAddress())
+        {
             qCWarning(viscaLog()) << "Failed to set address - init attempt:" << i + 1 << "out of" << INIT_TRIES_COUNT;
             continue;
         }
 
         if (executeCommand(ViscaCommands::Init::ifClear(), BASE_WAIT_TIME_MS * 2, "Clearing command buffer"))
+        {
             return;
+        }
 
         qCWarning(viscaLog()) << "Failed to clear command buffer - init attempt:" << i + 1 << "out of" << INIT_TRIES_COUNT;
         usleep(DEFAULT_USLEEP_WAIT);
@@ -21,20 +26,24 @@ Visca::Visca(const char *device_path) : m_uart(device_path) {
     qCWarning(viscaLog()) << "Error while initializing UART communication!";
 }
 
-bool Visca::setAddress() {
+bool Visca::setAddress()
+{
     std::array<uint8_t, 4> reply{};
-    if (!m_uart.sendMessage(addr::BROADCAST, ViscaCommands::Init::addressSet()) || !m_uart.receiveMessage(reply, BASE_WAIT_TIME_MS)) {
+    if (!m_uart.sendMessage(addr::BROADCAST, ViscaCommands::Init::addressSet())
+            || !m_uart.receiveMessage(reply, BASE_WAIT_TIME_MS))
+    {
         qCWarning(viscaLog()) << "Failed to set address.";
         return false;
     }
 
     m_camAddr = reply[2] - 1 + addr::CAM_BASE;
-
     qCInfo(viscaLog()) << "Address set succesfull, returned device address:" << Qt::hex << reply[2] - 1;
+
     return true;
 }
 
-void Visca::printReplyError(err code) {
+void Visca::printReplyError(err code)
+{
     switch (code) {
         case err::LENGTH:
             qCWarning(viscaLog()) << "Message length error";
